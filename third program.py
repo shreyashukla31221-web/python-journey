f809c2cefae6d8bb15b2872bf0bde6b115418818
Shreya Shukla <shreyashukla31221@gmail.com>
2026-09-06 21:47:43 +0530

    Add files via upload

---
+++ third program.py
@@ -0,0 +1,10 @@
+#"program to check whether is prime no. even or odd"
+n = int(input("Enter a prime number"))
+if n % 2 == 0:
+ print("Enter number is a even")
+else:
+ print("Enter number is odd")
+
+
+
+
